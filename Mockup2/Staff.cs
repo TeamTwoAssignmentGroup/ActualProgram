3d50a0e93f669b9f5525a0d0ed59a1ce497babe5
Roland <401627@edu.cwa.ac.uk>
2017-10-31 10:46:13 +0000

    Added Staff class

---
+++ Mockup2/Staff.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mockup2
+{
+    class Staff
+    {
+
+        int id;
+        string firstName;
+        string lastName;
+        string jobrole;
+        string email;
+        string password;
+      
+
+
+
+        
+
+        public int ID
+        {
+            set { id = value; }
+            get { return id; }
+        }
+
+
+        public string FirstName
+        {
+            set { firstName = value; }
+            get { return firstName; }
+        }
+
+        public string LastName
+        {
+            set { lastName = value; }
+            get { return lastName; }
+        }
+
+        public string Password
+        {
+            set { password = value; }
+            get { return password; }
+        }
+
+        public string JobRole
+        {
+            set { jobrole = value; }
+            get { return jobrole; }
+        }
+       
+        public string Email
+        {
+            set { email = value; }
+            get { return email; }
+        }
+
+
+
+
+    }
+}
