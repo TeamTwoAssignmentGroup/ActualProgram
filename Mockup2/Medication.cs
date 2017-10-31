0c1e40f5ba2e8cf4f7bc685cc19d664b439f2a02
Roland <401627@edu.cwa.ac.uk>
2017-10-31 10:50:08 +0000

    Added Medication class

---
+++ Mockup2/Medication.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mockup2
+{
+    class Medication
+    {
+        int id;
+        
+        string scientificname;
+        string commercialname;
+        string manufacturer;
+      
+
+
+      
+
+        public int ID
+        {
+            set { id = value; }
+            get { return id; }
+        }
+
+        public string ScientificName
+        {
+            set {scientificname = value; }
+            get { return scientificname; }
+        }
+
+        public string CommercialName
+        {
+            set { commercialname = value; }
+            get { return commercialname; }
+        }
+
+        public string Manufacturer
+        {
+            set { manufacturer = value; }
+            get { return manufacturer; }
+        }
+
+      
+      
+
+    }
+}
