4851c10476c0d3d8110f5d0becfa89802becd5f3
Bradley Shenton <354738@edu.cwa.ac.uk>
2017-11-29 09:58:25 +0000

    preparing to merge with factory changes

--- UnitTests/Brad.cs
+++ UnitTests/Brad.cs
@@ -1,17 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using Mockup2.Factories;
 using Mockup2.DatabaseClasses;
-using Mockup2.AdminForms;
-using Mockup2.TestCode;
+using Mockup2.Factories;
 
 namespace UnitTests
 {
@@ -19,15 +9,21 @@ namespace UnitTests
     public class Brad
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestStaffFactory()
         {
+            DBConnection dbCon = new DBConnection();
+            /// <summary>
+            /// Tests that <see cref="Mockup2.Factories.StaffFactory"/> returns correct information.
+            /// By pulling a <see cref="Mockup2.DatabaseClasses.Staff"/> by the ID of 2,
+            /// the first name should be 'King' and the last name should be 'Vernay'.
+            /// </summary>
+
+            StaffFactory pf = new StaffFactory(dbCon);
+            Staff p = pf.GetStaffByID(2)[0];
+            Assert.AreEqual(p.FirstName, "King");
+            Assert.AreEqual(p.LastName, "Vernay");
 
 
-        }
-
-        private void PopulateAdminFormRota()
-        {
-            
         }
     }
 }
